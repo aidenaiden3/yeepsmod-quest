d62e0a67d978f79bc2f019432394add06abac030
aidenaiden3 <davidsonaiden34@gmail.com>
2026-03-28 20:08:11 -0400

    Update and rename java to app/src/main/java/com/yeepsmod/quest/MainActivity.java

---
+++ app/src/main/java/com/yeepsmod/quest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.yeepsmod.quest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private static final int OVERLAY_PERMISSION_REQ = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ);
+            } else {
+                startOverlay();
+            }
+        } else {
+            startOverlay();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    startOverlay();
+                } else {
+                    Toast.makeText(this, "Overlay permission required!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private void startOverlay() {
+        Intent intent = new Intent(this, OverlayService.class);
+        startService(intent);
+        finish();
+    }
+}
